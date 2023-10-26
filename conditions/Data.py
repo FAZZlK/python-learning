8528cdfc09e870e0f8a5b54aeae078271ae050c7
FAZZlK <149055336+FAZZlK@users.noreply.github.com>
2023-10-26 15:56:56 +0300

    Data.py

--- conditions/Data.py
+++ conditions/Data.py
@@ -6,11 +6,23 @@
 months = ["Січень", "Лютий", "Березень", "Квітень", "Травень", "Червень", "Липень", "Серпень", "Вересень", "Жовтень", "Листопад", "Грудень"]
 seasons = ["Зима", "Весна", "Літо", "Осінь"]
 user_input = int(input("Введіть число від 1 до 12: "))
-    
-if 1 <= user_input <= 12:
-        month_name = months[user_input - 1]
-        season_index = (user_input - 12) // 3
-        season_name = seasons[season_index] 
-        print(f"{month_name}, {season_name}")
+season_index = 0
+
+
+if user_input == 12 or user_input == 1 or user_input == 2:
+   month_name = months[user_input - 1]
+   print(f"{month_name}, {seasons[season_index]}")
+elif 2 < user_input < 6:
+  month_name = months[user_input - 1]
+  season_index = 1
+  print(f"{month_name}, {seasons[season_index]}")
+elif 5 < user_input < 9:
+  month_name = months[user_input - 1]
+  season_index = 2
+  print(f"{month_name}, {seasons[season_index]}")
+elif 8 < user_input < 12:
+  month_name = months[user_input - 1]
+  season_index = 3
+  print(f"{month_name}, {seasons[season_index]}")
 else:
         print("Помилка")
